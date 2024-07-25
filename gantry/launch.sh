c68b33475c344976647f733dbf4647bce3bf984d
taidnguyen <tainguyen7597@gmail.com>
2024-07-25 12:07:23 +0000

    gantry checkpoint

--- gantry/launch.sh
+++ gantry/launch.sh
@@ -3,10 +3,12 @@ set -ex
 
 NUM_NODES=1
 
-ABLATION_GROUPS=("baseline" "no_code" "no_math" "no_flan" "no_wiki")
-# ABLATION_GROUPS=("baseline")
-# "olmo-86m"
-MODEL_SIZES=("olmo-508m" "olmo-156m" "olmo-11m")
+# ABLATION_GROUPS=("no_wiki" "baseline" "no_code" "no_math" "no_flan")
+# ABLATION_GROUPS=("no_code" "no_math" "no_flan")
+ABLATION_GROUPS=("no_flan" "no_math")
+# "olmo-156m" "olmo-11m" "olmo-86m" olmo-508m
+#TODO: 11m
+MODEL_SIZES=("olmo-86m")
 YMD="2024-07-21"
 
 # ABLATION_GROUPS=("no_math")
@@ -19,14 +21,11 @@ for MODEL in "${MODEL_SIZES[@]}"; do
     for GROUP in "${ABLATION_GROUPS[@]}"; do
         RUN_NAME="${GROUP}_${MODEL}"
         CONFIG_PATH="configs/tain/${YMD}/${GROUP}_${MODEL}.yaml"
-
-        # Tai: don't have permission for priority "high"
-        # Launch gantry
         gantry run \
-          --workspace ai2/tain\
+          --workspace ai2/cheap_decisions \
           --task-name data-decisions \
-          --description "cheap data decisions" \
-          --priority normal \
+          --description "${GROUP}_${MODEL}" \
+          --priority high \
           --preemptible \
           --beaker-image tain/olmo-tai \
           --cluster ai2/jupiter-cirrascale-2 \
@@ -40,7 +39,7 @@ for MODEL in "${MODEL_SIZES[@]}"; do
           --env LOG_FILTER_TYPE=local_rank0_only \
           --env OMP_NUM_THREADS=8 \
           --env OLMO_TASK=model \
-          --env-secret WANDB_API_KEY=WANDB_API_KEY \
+          --env-secret WANDB_API_KEY=WANDB_API_KEY_TAI \
           --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
           --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
           --shared-memory 10GiB \
@@ -50,7 +49,5 @@ for MODEL in "${MODEL_SIZES[@]}"; do
           --timeout=-1 \
           --allow-dirty \
           -- /bin/bash -c "gantry/train.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK ${CONFIG_PATH}" &
-
-        echo "Started training for tain/${YMD}/${GROUP}_${MODEL}"
     done
 done
